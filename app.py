0d27886001a0e27a062e2cf1c069a46548cf2837
Nishika0804 <gautamnishika8@gmail.com>
2026-04-15 00:40:27 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# Simple demo application
+API_KEY = "SECRET123"  # fake secret for detection
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Secure Pipeline"
+
+if __name__ == "__main__":
+    print(greet("DevOps"))
