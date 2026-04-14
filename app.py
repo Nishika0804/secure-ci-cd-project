cd8c8464a776ec7030c76b153ca9a0441031084a
Nishika0804 <gautamnishika8@gmail.com>
2026-04-15 00:48:51 +0530

    trigger secret detection

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Simple demo application
-API_KEY = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"  # fake secret for detection
+API_KEY = "ghp_abcdEFGHijklMNOPqrstUVWXyz1234567890" # fake secret for detection
 
 def greet(name):
     return f"Hello, {name}! Welcome to Secure Pipeline"
