0dd51815ebdf67f88fe005b03f1ff6ee0b0ac0fb
Nishika0804 <gautamnishika8@gmail.com>
2026-04-15 00:51:46 +0530

    force detection

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Simple demo application
-API_KEY = "ghp_abcdEFGHijklMNOPqrstUVWXyz1234567890" # fake secret for detection
+AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE" # fake secret for detection
 
 def greet(name):
     return f"Hello, {name}! Welcome to Secure Pipeline"
